290c6453aace4bf04132829629ca454438d1e4d5
gabe351 <gabrielrosadn@gmail.com>
2016-06-08 15:26:38 -0300

    Commit inicial

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ActiveRecord::Base
+  validates_presence_of :full_name, :email, :password, :location
+  validates_confirmation_of :password
+  validates_length_of :bio, :minimum => 30, :allow_blank => false
+  validates_format_of :email, :with => /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/
+  validates_uniqueness_of :email
+end
